556d6225bca4aa9a982d5842f2526e3a1af29bfe
Eoin McGrath <eoin.mcgrath@dell.com>
2026-08-21 11:49:53 +0100

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -33,11 +33,10 @@ async function getAlbInfo() {
   );
 
   return {
-    loadBalancer: alb.LoadBalancerArn.split('loadbalancer/')[1],
-    targetGroup: tg.TargetGroupArn.split(':targetgroup/')[1]
+  loadBalancer: alb.LoadBalancerArn.split('loadbalancer/')[1],
+  targetGroup: `targetgroup/${tg.TargetGroupName}/${tg.TargetGroupArn.split('/').pop()}`
   };
 }
-
 
 // Helper: fetch + sort CloudWatch data
 async function getMetric(params) {
@@ -179,8 +178,51 @@ app.get('/alb-requests', async (req, res) => {
 
 app.get('/health', async (req, res) => {
   try {
-    const info = await getAlbInfo();
-    res.json(info);
+    const { loadBalancer, targetGroup } = await getAlbInfo();
+
+    const healthy = await getMetric({
+      Namespace: 'AWS/ApplicationELB',
+      MetricName: 'HealthyHostCount',
+      Dimensions: [
+        {
+          Name: 'LoadBalancer',
+          Value: loadBalancer
+        },
+        {
+          Name: 'TargetGroup',
+          Value: targetGroup
+        }
+      ],
+      StartTime: new Date(Date.now() - 3600000),
+      EndTime: new Date(),
+      Period: 300,
+      Statistics: ['Average']
+    });
+
+    const unhealthy = await getMetric({
+      Namespace: 'AWS/ApplicationELB',
+      MetricName: 'UnHealthyHostCount',
+      Dimensions: [
+        {
+          Name: 'LoadBalancer',
+          Value: loadBalancer
+        },
+        {
+          Name: 'TargetGroup',
+          Value: targetGroup
+        }
+      ],
+      StartTime: new Date(Date.now() - 3600000),
+      EndTime: new Date(),
+      Period: 300,
+      Statistics: ['Average']
+    });
+
+    res.json({
+      healthy,
+      unhealthy
+    });
+
   } catch (err) {
     res.status(500).send(err);
   }
